3ff248c5710cb51855305c81da3905944e2de4a4
Dunga Tejas <165931026+Tejas3157@users.noreply.github.com>
2025-10-09 17:59:38 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import './components/Navbar'
-import './components/Footer'
+import Home from './components/Home';
 import About from './components/About';
 import Skills from './components/Skills';
 import Contact from './components/Contact';
@@ -13,28 +13,13 @@ import Card from './components/Card';
 function App() {
   return (
     <div className="App">
-    <BrowserRouter>
-    <Navbar/>
-    
-    <Routes>
-      <Route path='/' element={
-        <>
-          <About/>
-          <Skills/>
-          <Contact/>
-          <Card/>
-        </>
-      }/>
-  
-      <Route path='/About' element={<About />} />
-      <Route path='/Skills' element={<Skills />} />
-      <Route path='/Contact' element={<Contact />} />
-      <Route path='/Card' element={<Card />} />
-   
-    </Routes>
-    <Footer/>
-    </BrowserRouter>
-      
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/About' element={<About />} />
+        <Route path='/Skills' element={<Skills />} />
+        <Route path='/Contact' element={<Contact />} />
+        <Route path='/Card' element={<Card />} />
+      </Routes>
     </div>
   );
 }
